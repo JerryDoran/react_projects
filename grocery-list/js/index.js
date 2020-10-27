cc7493897ead58ac18416f5dcbfba6a92f8aa9b6
Jerry Doran <Jerry.Doran@pccairfoils.com>
2020-10-27 11:12:05 -0400

    finished grocery bud project with local storage

--- grocery-list/js/index.js
+++ grocery-list/js/index.js
@@ -7,10 +7,19 @@ const UserContext = React.createContext();
 const ChannelContext = React.createContext();
 const CountContext = React.createContext();
 
+const getLocalStorage = () => {
+  let list = localStorage.getItem('groceryList');
+  if (list) {
+    return JSON.parse(localStorage.getItem('groceryList'));
+  } else {
+    return [];
+  }
+};
+
 // App Component
 const App = () => {
   const [name, setName] = useState('');
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getLocalStorage());
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({
@@ -34,7 +43,18 @@ const App = () => {
       // display alert
       showAlert(true, 'danger', 'please enter value');
     } else if (name && isEditing) {
-      // deal with edit
+      setList(
+        list.map((item) => {
+          if (item.id === editID) {
+            return { ...item, title: name };
+          }
+          return item;
+        })
+      );
+      setName('');
+      setEditID(null);
+      setIsEditing(false);
+      showAlert(true, 'success', 'value changed');
     } else {
       showAlert(true, 'success', 'item added to the list');
       const newItem = { id: new Date().getTime().toString(), title: name };
@@ -43,10 +63,26 @@ const App = () => {
     }
   };
 
+  const removeItem = (id) => {
+    showAlert(true, 'danger', 'item removed');
+    setList(list.filter((item) => item.id !== id));
+  };
+
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditID(id);
+    setName(specificItem.title);
+  };
+
+  useEffect(() => {
+    localStorage.setItem('groceryList', JSON.stringify(list));
+  }, [list]);
+
   return (
     <section className='section-center'>
       <form className='grocery-form' onSubmit={handleSubmit}>
-        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
         <h3>grocery bud</h3>
         <div className='form-control'>
           <input
@@ -63,7 +99,7 @@ const App = () => {
       </form>
       {list.length > 0 && (
         <div className='grocery-container'>
-          <List items={list} />
+          <List items={list} removeItem={removeItem} editItem={editItem} />
           <button className='clear-btn' onClick={clearList}>
             clear items
           </button>
@@ -74,7 +110,7 @@ const App = () => {
 };
 
 //  component
-const List = ({ items }) => {
+const List = ({ items, removeItem, editItem }) => {
   return (
     <div className='grocery-list'>
       {items.map((item) => {
@@ -83,10 +119,18 @@ const List = ({ items }) => {
           <article key={id} className='grocery-item'>
             <p className='title'>{title}</p>
             <div className='btn-container'>
-              <button type='button' className='edit-btn'>
+              <button
+                type='button'
+                className='edit-btn'
+                onClick={() => editItem(id)}
+              >
                 <i className='fas fa-edit'></i>
               </button>
-              <button type='button' className='delete-btn'>
+              <button
+                type='button'
+                className='delete-btn'
+                onClick={() => removeItem(id)}
+              >
                 <i className='fas fa-trash'></i>
               </button>
             </div>
@@ -98,13 +142,13 @@ const List = ({ items }) => {
 };
 
 //  component
-const Alert = ({ type, msg, removeAlert }) => {
+const Alert = ({ type, msg, removeAlert, list }) => {
   useEffect(() => {
     const timeout = setTimeout(() => {
       removeAlert();
-    }, 3000);
+    }, 1500);
     return () => clearTimeout(timeout);
-  }, []);
+  }, [list]);
   return <p className={`alert alert-${type}`}>{msg}</p>;
 };
 
