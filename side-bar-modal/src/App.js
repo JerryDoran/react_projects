c606574147675f228c1feb1ec730ff69ae170a9f
Jerome Doran <disneyfan9184@gmail.com>
2020-11-08 09:40:35 -0500

    finished sidebar modal project

---
+++ side-bar-modal/src/App.js
@@ -0,0 +1,15 @@
+import Home from './components/Home';
+import Modal from './components/Modal';
+import Sidebar from './components/Sidebar';
+
+function App() {
+  return (
+    <div className='App'>
+      <Home />
+      <Modal />
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
